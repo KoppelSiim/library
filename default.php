e6372bbd12b0c02b37f4c468cce88e8d0e05a9fb
Ann-JK <annabelkongas@gmail.com>
2023-10-17 21:33:02 +0300

    search form and php base

--- default.php
+++ default.php
@@ -1,3 +1,45 @@
+<?php 
+require($_SERVER["DOCUMENT_ROOT"]."/../library_config.php");
+global $connect;
+
+// otsing
+$baseSelect = "SELECT * FROM books ";
+
+//isSet otsingu submit
+//  ifelse blokid tyhjade parameetrite kontrollimiseks
+//  sql käsu täitmine
+//  andmete välja kuvamine vastavalt sql käsule
+
+
+?>
+
+<form class="searchForm" method="POST" action="">
+
+    <div class="form-group">
+        <label for="title" class="form-label">Pealkiri</label>
+        <input type="text" class="form-control" id="title" name="title" style="max-width: 200px">
+    </div>
+
+    <div class="form-group">
+        <label for="author" class="form-label">Autor</label>
+        <input type="text" class="form-control" id="author" name="author" style="max-width: 200px">
+    </div>
+    
+    <div class="form-group">
+        <label for="subscribe">Kuva ainult saadaval raamatuid </label>
+        <input type="checkbox" name="available" id="available">
+        <input type="submit" value="available">
+    </div> 
+
+    <div class="form-group">
+        <button type="submit" name="submitSearch">Otsi</button>
+    </div>
+
+</form>
+
+
+
+
 <p>Pealeht</p>
 
 Raamatute otsing
