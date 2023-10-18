a278938b1f289f907e676df00b140d779eb0f883
Siim Koppel <siimk999@gmail.com>
2023-10-18 15:40:59 +0300

    Add book view, delete book

--- admin.php
+++ admin.php
@@ -2,7 +2,8 @@
 <div class="container">
     <h1>Haldus</h1>
     <!--Raamatu lisamine form -->
-    <form method="POST" action="">
+    <h4>Lisa raamat</h4>
+    <form method="POST" action="" class="mb-2">
         <div class="form-group row col-2 mb-3">
             <label for="bookTitle" class="form-label">Pealkiri</label>
             <input type="text" class="form-control" id="bookTitle" name="title">
@@ -15,4 +16,36 @@
             <button type="submit" name="addBook" class="btn btn-primary">Salvesta</button>
         </div>
     </form>
+
+    <!-- Kuva kõik raamatud, kustuta -->
+    <!-- Todo col suurus responsive, gap vaiksemaks -->
+    <h4>Raamatud</h4>
+    
+    <div class="row">
+        <div class="col-2">
+            <label for="bookTitle" class="">Pealkiri:</label>
+        </div>
+        <div class="col-2">
+            <p id="bookTitle">titleofbook</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-2">
+            <label for="bookAuthor" class="">Autor:</label>
+        </div>
+        <div class="col-2">
+            <p id="bookAuthor">authorofbook</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-2">
+            <form method="POST" action="">
+                <!-- id kustutamiseks saadame hiljem siit -->
+                <button type="submit" name="deleteBook" value = "$id" class="btn btn-danger mb-2">Kustuta</button>
+            </form>
+        </div>  
+    </div>
+
 </div>
