db19ccffc5bae0d0241994951ebba44621de00e4
Siim Koppel <siimk999@gmail.com>
2023-10-25 18:04:16 +0300

    Update user, implement return book, update erd

--- user.php
+++ user.php
@@ -1,37 +1,48 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/../library_config.php");
+global $connect;
+// Otsime laenatud raamatud ainult
+$sqlGetLoanedBooks = $connect->prepare("SELECT id, title, author, deadline FROM books WHERE status ='loaned'");
+$sqlGetLoanedBooks->bind_result($id, $title, $author,$deadLine);
+$sqlGetLoanedBooks->execute();
+?>
+
 <!--Kasutaja vaade --->
 <div class="container">
     <h1>Kasutaja vaade</h1>
-    <h4>Laenatud raamatud</h4>
+    <h4 style="margin-bottom:20px;">Laenatud raamatud</h4>
    
     <div class="row">
-        <div class="col-2 mb-2">
-            <label for="bookTitle" class="">Pealkiri</label>
+        <div class="col-3">
+            <label class="mb-2" style="font-weight:bold;">Pealkiri</label>
         </div>
-        <div class="col-2 mb-2">
-            <label for="bookAuthor" class="">Autor</label>
+        <div class="col-2">
+            <label class="mb-2" style="font-weight:bold;">Autor</label>
         </div>
-        <div class="col-2 mb-2">
-            <label for="deadLine" class="">Tähtaeg</label>
+        <div class="col-2">
+            <label class="mb-2" style="font-weight:bold;">Tähtaeg</label>
         </div>
     </div>
-    
-     <!-- Siia tuleb hakata fetchima raamatuid -->
-    <div class="row"> 
-        <div class="col-2">
-            <p id="bookTitle">Titleofbook</p>
-        </div>
-        <div class="col-2">
-            <p id="bookAuthor">Authorofbook</p>
-        </div>
-        <div class="col-2">
-            <p id="deadLine">Insert date here</p>
-        </div>
-        <div class="col-2">
-            <form method="POST" action="">
-                <!-- id tagastamiseks saadame hiljem siit -->
-                <button type="submit" name="deleteBook" value = "$id" class="btn-sm btn-primary mb-4">Tagasta</button>
-            </form>
-        </div>  
-    </div>
+    <hr>
+    <!-- Kuvame laenutatud raamatud: pealkiri, autor, tahtaeg ja tagasta nupp -->
+    <!-- Tagasta nupp muudab konkreetse raamatu staatuse: loaned->shelf  ja deadline-> NULL -->
+    <?php
+    while($sqlGetLoanedBooks ->fetch()) {
+        echo '
+        <div class="row"> 
+            <div class="col-3">' . htmlspecialchars($title) . '</div>
+            <div class="col-2">' . htmlspecialchars($author) . '</div>
+            <div class="col-2">' . htmlspecialchars($deadLine) . '</div>
+            <div class="col-2">
+                <form method="POST" action="return_book.php">
+                    <input type="hidden" name="bookId" value=' . htmlspecialchars($id) .'>
+                    <button type="submit" name="returnBook" class="btn-sm btn-primary mb-4">Tagasta</button>
+                </form>
+            </div>
+        </div>';  
+    }
+    $sqlGetLoanedBooks->close();
+    $connect->close();
+    ?>
 
 </div>
