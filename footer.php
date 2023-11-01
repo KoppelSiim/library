af2d6425ce3d52eff8a8926750df1d7d8d7b2a32
Siim Koppel <siimk999@gmail.com>
2023-11-01 18:48:39 +0200

    Glass effect footer, body gray bgcolor

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer id="mainFooter" class="container bg-light d-flex align-items-center justify-content-center">
+<footer id="mainFooter" class="container bg-light-opacity d-flex align-items-center justify-content-center">
   <div class="container-fluid mt-auto">
     <div class="row">
       <p class="text-center">Lehe tegid Siim Koppel & Annabel Jakubel</p>
